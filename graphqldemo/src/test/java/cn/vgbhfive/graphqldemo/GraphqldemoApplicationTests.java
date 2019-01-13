7fa61367a2701fb7695077a4780496d61b534e4d
vgbhfive <vgbhfive@foxmail.com>
2019-01-13 19:27:05 +0800

    GraphQL Demo Charapter One

---
+++ graphqldemo/src/test/java/cn/vgbhfive/graphqldemo/GraphqldemoApplicationTests.java
@@ -0,0 +1,17 @@
+package cn.vgbhfive.graphqldemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GraphqldemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
